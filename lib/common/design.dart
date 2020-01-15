0d35391923228abf146e4fe321456a9972cb8dc1
Jimmy Pham <jpham35@gatech.edu>
2020-01-15 14:31:05 -0500

    Set up color palette

---
+++ lib/common/design.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Design {
+  ///Example color palette, delete when we have ours
+  static Color blueSky = Color(0xFF068FFA);
+  static Color greenLand = Color(0xFF89ED91);
+  static Color blueSkyLight = Color(0x40068FFA);
+  static Color greenLandLight = Color(0x4089ED91);
+  static Color blueSkyLighter = Color(0x10068FFA);
+}
