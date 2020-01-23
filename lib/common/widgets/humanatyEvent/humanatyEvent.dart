3a5db009c94a3d2898253192ceac296f38bb99de
Jimmy Pham <jpham35@gatech.edu>
2020-01-23 18:35:34 -0500

    Created reusable event widget that currently takes in name, date, and description of event

---
+++ lib/common/widgets/humanatyEvent/humanatyEvent.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HumanatyEvent extends StatelessWidget {
+  HumanatyEvent(
+    {
+      this.eventName,
+      this.eventDate,
+      this.eventDescription
+    }
+  );
+
+  final String eventName;
+  final String eventDate;
+  final String eventDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(16.0),
+
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.event)
+                  ],
+                )
+              ),
+              Expanded(
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(eventName),
+                    Text(eventDate),
+                    Text(eventDescription),
+                  ],
+                )
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
