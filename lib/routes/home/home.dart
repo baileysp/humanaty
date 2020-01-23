562696e5aacfc4b87b8194609e8f356777a62a4a
Jimmy Pham <jpham35@gatech.edu>
2020-01-23 15:08:14 -0500

    BottomNavigationBar functional but not done, starting home screen development

--- lib/routes/home/home.dart
+++ lib/routes/home/home.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
-/*
-  TODO: Set up body parameter so it switches to correct page (navigation/routing)
- */
+import 'package:humanaty/common/widgets.dart';
+import 'package:humanaty/routes/_router.dart';
+
+//Screen/Widget that is displayed for the Home page is the 'Current' class
+class Current extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: HumanatyDrawer(),
+      body: Stack(
+        children: <Widget>[
+          // Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: NetworkImage('https://images.unsplash.com/photo-1517030330234-94c4fb948ebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80'),
+          //       fit: BoxFit.cover
+          //     )
+          //   ),
+          // )
+          Padding(
+            padding: EdgeInsets.all(60),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "Humanaty",
+                style: TextStyle(fontSize: 30),
+              ),
+            )
+          )
+        ],
+      )
+    );
+  }
+}
+
 class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
@@ -10,10 +42,11 @@ class HomeState extends State<Home> {
   int navIndex = 0;
   static const TextStyle navStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   final _navigationOptions = [
-    Text("Home", style: navStyle,),
-    Text("Settings", style: navStyle,),
-    Text("Map", style: navStyle,),
-    Text("Map2", style: navStyle,),
+    Current(),
+    Settings(),
+    Settings(),
+    Map(),
+    Map()
   ];
   
   @override
