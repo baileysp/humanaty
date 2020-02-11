0a08e59ed8e5fb2484b8e5dcc7e71346267c42d7
Jimmy Pham <jpham35@gatech.edu>
2020-02-11 16:10:46 -0500

    Events now navigate to a new page and pass in event details

--- lib/routes/events/events.dart
+++ lib/routes/events/events.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Events extends StatelessWidget {
+  
+  Events(
+    {
+      this.eventName,
+      this.eventDate,
+      this.eventDescription,
+    }
+  );
+
+  String eventName;
+  String eventDate;
+  String eventDescription;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hooah"),
+        title: Text(this.eventName),
       ),
       body: Center(
-        child: Text("My events coming soon"),
+        child: Text(this.eventDescription),
       ),
     );
   }
